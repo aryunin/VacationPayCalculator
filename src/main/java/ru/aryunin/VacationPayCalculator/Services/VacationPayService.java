f71bfc159e764b92126354018965ca6077637445
Artem Yunin <im.aryunin@gmail.com>
2022-10-07 18:14:40 +0300

    calculating added

--- src/main/java/ru/aryunin/VacationPayCalculator/Services/VacationPayService.java
+++ src/main/java/ru/aryunin/VacationPayCalculator/Services/VacationPayService.java
@@ -1,17 +1,34 @@
 package ru.aryunin.VacationPayCalculator.Services;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.aryunin.VacationPayCalculator.Models.VacationPay;
+import ru.aryunin.VacationPayCalculator.Utils.WorkingDaysCalendar;
 
-import java.util.Date;
+import java.util.*;
 
 @Service
+@AllArgsConstructor
 public class VacationPayService {
+    private final WorkingDaysCalendar workingDaysCalendar;
+
+    private int getWorkingDaysCount(Date startDate, Date endDate) {
+        int counter = 0;
+        workingDaysCalendar.setDate(startDate);
+        while (!workingDaysCalendar.getDate().after(endDate)) {
+            if(workingDaysCalendar.isWorkingDay()) counter++;
+            workingDaysCalendar.nextDay();
+        }
+        workingDaysCalendar.setDate(new Date());
+        return counter;
+    }
+
     public VacationPay calculate(Date startDate,
                                  Date endDate,
                                  double averageSalary) {
         VacationPay vacationPay = new VacationPay();
-        vacationPay.setAmount(0); // TODO
+        int workingDaysCount = getWorkingDaysCount(startDate, endDate);
+        vacationPay.setAmount((averageSalary/29.3)*workingDaysCount);
         return vacationPay;
     }
 }
