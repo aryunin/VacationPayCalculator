57f4c76c8d459ab0234877b91624aa53db876932
Artem Yunin <im.aryunin@gmail.com>
2022-10-07 13:50:56 +0300

    DateTimeFormat annotation removed

--- src/main/java/ru/aryunin/VacationPayCalculator/Services/VacationPayService.java
+++ src/main/java/ru/aryunin/VacationPayCalculator/Services/VacationPayService.java
@@ -1,6 +1,5 @@
 package ru.aryunin.VacationPayCalculator.Services;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 import ru.aryunin.VacationPayCalculator.Models.VacationPay;
 
@@ -8,8 +7,8 @@ import java.util.Date;
 
 @Service
 public class VacationPayService {
-    public VacationPay calculate(@DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
-                                 @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
+    public VacationPay calculate(Date startDate,
+                                 Date endDate,
                                  double averageSalary) {
         VacationPay vacationPay = new VacationPay();
         vacationPay.setAmount(0); // TODO
