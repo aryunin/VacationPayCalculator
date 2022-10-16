ff6050d8eabbca9026853bcdd7e6ef33770481cb
Artem Yunin <im.aryunin@gmail.com>
2022-10-16 19:37:59 +0300

    Data classes are made serializable

--- src/main/java/ru/aryunin/VacationPayCalculator/DTO/VacationPayDTO.java
+++ src/main/java/ru/aryunin/VacationPayCalculator/DTO/VacationPayDTO.java
@@ -4,9 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @NoArgsConstructor
 @Getter
 @Setter
-public class VacationPayDTO {
-    double amount;
+public class VacationPayDTO implements Serializable {
+
+    private static final long serialVersionUID = 7714483778358449356L;
+    private double amount;
 }
--- src/main/java/ru/aryunin/VacationPayCalculator/Models/VacationPay.java
+++ src/main/java/ru/aryunin/VacationPayCalculator/Models/VacationPay.java
@@ -4,9 +4,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @NoArgsConstructor
 @Getter
 @Setter
-public class VacationPay {
-    double amount;
+public class VacationPay implements Serializable {
+    private static final long serialVersionUID = 2168369073827051629L;
+    private double amount;
 }
