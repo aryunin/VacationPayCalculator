9f81c4e132ca4f33eba5f04d746c51740d66d9da
aryunin <im.aryunin@gmail.com>
2023-07-19 22:41:51 +0300

    Exceptions are thrown

--- src/main/java/com/aryunin/VacationPayCalculator/controller/VacationPayController.java
+++ src/main/java/com/aryunin/VacationPayCalculator/controller/VacationPayController.java
@@ -1,5 +1,7 @@
 package com.aryunin.VacationPayCalculator.controller;
 
+import com.aryunin.VacationPayCalculator.exception.NegativeDaysException;
+import com.aryunin.VacationPayCalculator.exception.NegativeSalaryException;
 import com.aryunin.VacationPayCalculator.service.VacationPayService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -18,6 +20,8 @@ public class VacationPayController {
     private String calculate(@RequestParam Double salary,
                              @RequestParam Integer days,
                              @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate) {
+        if(salary < 0) throw new NegativeSalaryException();
+        if(days < 0) throw new NegativeDaysException();
         return null;
     }
 }
