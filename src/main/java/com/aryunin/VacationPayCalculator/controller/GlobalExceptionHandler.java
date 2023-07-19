a78ffd8abbc134aa4ec81197ce8f2178d5ba479f
aryunin <im.aryunin@gmail.com>
2023-07-20 00:33:17 +0300

    logging added

--- src/main/java/com/aryunin/VacationPayCalculator/controller/GlobalExceptionHandler.java
+++ src/main/java/com/aryunin/VacationPayCalculator/controller/GlobalExceptionHandler.java
@@ -2,12 +2,14 @@ package com.aryunin.VacationPayCalculator.controller;
 
 import com.aryunin.VacationPayCalculator.exception.NegativeDaysException;
 import com.aryunin.VacationPayCalculator.exception.NegativeSalaryException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     private final String prefix = "ERROR: ";
 
@@ -16,6 +18,7 @@ public class GlobalExceptionHandler {
         ProblemDetail body = ProblemDetail
                 .forStatusAndDetail(HttpStatus.BAD_REQUEST, prefix + ex.getMessage());
         body.setTitle("Invalid days format");
+        log.info("handled exception: " + ex.getMessage());
         return body;
     }
 
@@ -24,6 +27,7 @@ public class GlobalExceptionHandler {
         ProblemDetail body = ProblemDetail
                 .forStatusAndDetail(HttpStatus.BAD_REQUEST, prefix + ex.getMessage());
         body.setTitle("Invalid salary format");
+        log.info("handled exception: " + ex.getMessage());
         return body;
     }
 }
--- src/main/java/com/aryunin/VacationPayCalculator/service/VacationPayService.java
+++ src/main/java/com/aryunin/VacationPayCalculator/service/VacationPayService.java
@@ -1,20 +1,28 @@
 package com.aryunin.VacationPayCalculator.service;
 
 import com.aryunin.VacationPayCalculator.util.ProductionCalendar;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
+@Slf4j
 public class VacationPayService {
     private final double avgDaysInMonth = 29.3;
 
     public double calculate(double salary, int days) {
-        return salary / avgDaysInMonth * days;
+        double result = salary / avgDaysInMonth * days;
+        log.info("calculation result (without date) is " + result);
+        return result;
     }
 
     public double calculate(double salary, int days, LocalDate startDate) {
         LocalDate endDate = startDate.plusDays(days);
-        return salary / avgDaysInMonth * (ProductionCalendar.countWorkingDays(startDate, endDate));
+        int workingDays = ProductionCalendar.countWorkingDays(startDate, endDate);
+        log.info("count of working days: " + workingDays);
+        double result = salary / avgDaysInMonth * (workingDays);
+        log.info("calculation result (with date) is " + result);
+        return result;
     }
 }
