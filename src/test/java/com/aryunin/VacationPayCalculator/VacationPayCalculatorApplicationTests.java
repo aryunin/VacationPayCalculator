602a0de035be8eab89dd3e5309b0934084055e57
aryunin <im.aryunin@gmail.com>
2023-07-19 21:47:46 +0300

    Initial commit

---
+++ src/test/java/com/aryunin/VacationPayCalculator/VacationPayCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryunin.VacationPayCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPayCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
